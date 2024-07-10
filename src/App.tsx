bd2a2e3c79dc0a59cc78f55e5e5604bb6946b517
João Victor Escandiel Bertolin <joaoescandielb@gmail.com>
2024-07-10 11:34:36 -0300

    fix(imports): removed module name replacement

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/ui/Header";
 import PaymentMethod from "./pages/PaymentMethod";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<PaymentMethod />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
