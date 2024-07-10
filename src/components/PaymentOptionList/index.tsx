52ec4cf7fd3e3351cf380f8827cab33e8c08d5ab
João Victor Escandiel Bertolin <joaoescandielb@gmail.com>
2024-07-10 19:19:50 -0300

    fix: payment-option-card-list re-styled

--- src/components/PaymentOptionList/index.tsx
+++ src/components/PaymentOptionList/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PaymentOptionCard from "../../components/ui/PaymentOptionCard";
 import data from "../../mocks/db.json";
 import TopLabel from "../ui/TopLabel";
@@ -5,12 +6,26 @@ import "./index.css";
 
 const PaymentOptionList = () => {
   const installments = data.paymentMethods.installments.slice(1);
+  const [checkedItems, setCheckedItems] = useState<number[]>([]);
+
+  const handleCheckboxChange = (id: number, checked: boolean) => {
+    setCheckedItems((prevCheckedItems) =>
+      checked
+        ? [...prevCheckedItems, id]
+        : prevCheckedItems.filter((item) => item !== id)
+    );
+  };
 
   return (
     <div className="payment-option-list">
       <TopLabel label="Pix Parcelado" />
       {installments.map((installment) => (
-        <div key={installment.id} className="payment-option-item">
+        <div
+          key={installment.id}
+          className={`payment-option-item ${
+            checkedItems.includes(installment.id) ? "checked" : ""
+          }`}
+        >
           <PaymentOptionCard
             installments={installment.label}
             amount={installment.amount}
@@ -21,6 +36,10 @@ const PaymentOptionList = () => {
             highlight={installment.highlight || ""}
             highlightValue={installment.highlightValue || ""}
             useListCard={true}
+            checked={checkedItems.includes(installment.id)}
+            onChange={(checked) =>
+              handleCheckboxChange(installment.id, checked)
+            }
           />
         </div>
       ))}
