f9e18fd939790411bcc0e02fbee4566b4011e4b9
João Victor Escandiel Bertolin <joaoescandielb@gmail.com>
2024-07-11 02:07:11 -0300

    feat: title for the credit-card-installments page

--- src/pages/CreditCardInstallments/index.tsx
+++ src/pages/CreditCardInstallments/index.tsx
@@ -1,5 +1,28 @@
+import { useLocation } from "react-router-dom";
+import Title from "../../components/ui/Title";
+
 const CreditCardInstallments = () => {
-  return <p>hello worllds </p>;
+  const location = useLocation();
+  const payment = location.state?.payment;
+
+  if (!payment) {
+    return (
+      <div className="flex-center">
+        Erro: Nenhuma informação de pagamento disponível.
+      </div>
+    );
+  }
+
+  const totalInstallments = parseInt(payment.label.replace("x", ""));
+  const remainingInstallments = totalInstallments - 1;
+
+  return (
+    <div className="flex-center title-container-confirm-payment">
+      <Title
+        text={`João, pague o restante em ${remainingInstallments}x no cartão`}
+      />
+    </div>
+  );
 };
 
 export default CreditCardInstallments;
